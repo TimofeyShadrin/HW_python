66c9d4f2facf7d49f7969c96d7324b1250047d1d
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-06 17:49:32 +0300

    Task 3 of seminar 2 finished

---
+++ sem_02/task3.py
@@ -0,0 +1,36 @@
+# Задайте список из n чисел последовательности (1 + 1/n) ** n и
+# выведите на экран их сумму.
+
+work = []
+def Order(number: int = 1):
+    global work
+    for i in range(1, number + 1):
+        work.append(round(((1 + 1 / i) ** i), 2))
+
+back = True
+total = 0
+def Result(digit: int):
+    global total
+    global back
+    if digit < 0:
+        digit = abs(digit)
+        Order(digit)
+        total = round(sum(work), 2)
+        back = True
+    elif digit == 0:
+        print('Your input is incorrect! Try again.')
+        back = False
+    else:
+        Order(digit)
+        total = round(sum(work), 2)
+        back = True
+
+while True:
+    try:
+        value = int(input('Please enter integer number: '))
+        Result(value)
+        if back == True:
+            print(f'The sum of all elements of list is: {total}')
+            break
+    except:
+        print('Your input is incorrect! Try again.')
