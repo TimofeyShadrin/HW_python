e5af4a8756d013146b664bf4e0299b1b77051e56
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-06 21:25:35 +0300

    Task 5 with algorithm of seminar 2 finished

--- sem_02/task5.py
+++ sem_02/task5.py
@@ -1,8 +1,45 @@
 # Реализуйте алгоритм перемешивания списка.
 import random
 
-
-order = list(range(0, 10))
+order = list(range(30, 50))
 print(f'Initial list: {order}')
-random.shuffle(order)
-print(f'Shuffled list: {order}')
+
+
+def check(value=' ', test=[]):
+    back = False
+    for digit in test:
+        if value == digit:
+            back = True
+            break
+    return back
+
+
+positions = []
+
+
+def indexes():
+    global positions
+    global order
+    size = 1
+    while size <= len(order):
+        temp = random.randint(0, len(order) - 1)
+        if not check(temp, positions):
+            positions.append(temp)
+            size += 1
+
+
+product = []
+
+
+def result():
+    global order
+    global positions
+    global product
+    for i in positions:
+        product.append(order[i])
+
+
+indexes()
+print(f'Mix indexes: {positions}')
+result()
+print(f'List have been shuffled: {product}')
