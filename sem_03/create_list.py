78609dcb4db0cc022dc8acc0b16daff6b89144aa
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-10 12:55:18 +0300

    Task3 of seminar 3 finished

--- sem_03/create_list.py
+++ sem_03/create_list.py
@@ -7,3 +7,11 @@ def new_list(min_size: int, max_size: int, min_value: int, max_value: int):
     for _ in range(size):
         numbers.append(random.randint(min_value, max_value))
     return numbers
+
+
+def new_list(min_size: int, max_size: int, min_value: float, max_value: float, accuracy: int):
+    size = random.randint(min_size, max_size)
+    numbers = []
+    for _ in range(size):
+        numbers.append(round(random.uniform(min_value, max_value), accuracy))
+    return numbers
---
+++ sem_03/task3.py
@@ -0,0 +1,21 @@
+# Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между максимальным
+# и минимальным значением дробной части элементов.
+#
+# Пример:
+#
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+from shlex import join
+
+import create_list as cl
+
+numbers = cl.new_list(4, 7, 0.00, 10.00, 2)
+print(numbers)
+
+for i in range(len(numbers)):
+    fraction = str(numbers[i]).split('.')[1]
+    numbers[i] = float('0.' + str(fraction))
+print(numbers)
+
+result = max(numbers) - min(numbers)
+print(round(result, 2))
