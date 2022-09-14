bcb623cdc764153a5e2e6ea763e8c44e2e935bfd
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-14 10:27:19 +0300

    'Task 1 of seminar 4 fixed

--- sem_04/task1.py
+++ sem_04/task1.py
@@ -6,10 +6,12 @@ digit = input('Введите число: ').split('.')
 accuracy = input('Введите точность в виде 0.001: ').split('.')
 result = ''
 
-if len(digit[1]) >= len(accuracy[1]):
-    result = round(float(digit[0] + '.' + digit[1][0:len(accuracy[1]) + 1]), len(accuracy[1]))
-else:
-    result = '.'.join(digit)
-    for _ in range(len(accuracy[1]) - len(digit[1])):
-        result += '0'
+while len(digit[1]) != len(accuracy[1]):
+    if len(digit[1]) >= len(accuracy[1]):
+        result = str(round(float(digit[0] + '.' + digit[1][0:len(accuracy[1]) + 1]), len(accuracy[1])))
+    else:
+        result = '.'.join(digit)
+        for _ in range(len(accuracy[1]) - len(digit[1])):
+            result += '0'
+    digit = result.split('.')
 print(result)
