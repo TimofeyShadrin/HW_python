f963b6e8b323918c29c93c541fcbedbb3a47a915
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-15 19:28:38 +0300

    Task 1 of seminar 5 are finished

---
+++ sem_05/task1.py
@@ -0,0 +1,5 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+str = input('Enter string: ').replace('абв', '').split(' ')
+result = ' '.join(list(filter(lambda x: x != '', str)))
+print(result)
