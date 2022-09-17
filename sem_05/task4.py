75fc406531ffdd13ac86f096a986e864110e3b9c
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-17 18:08:47 +0300

    Dict in task 4 of seminar 5 finished

--- sem_05/task4.py
+++ sem_05/task4.py
@@ -5,13 +5,53 @@ import json
 
 text = ''
 
-def load():
+
+def load(filename='text.json'):
     try:
         global text
-        with open("text.json", "r", encoding="utf-8") as fh:
+        with open(filename, "r", encoding="utf-8") as fh:
             text = json.load(fh)
         print("File loaded!")
     except:
         print('Your file not found!')
 
+
+dic = {}
+words = []
+
+
+def compress():
+    global text
+    global dic
+    global words
+    words = text.strip().split()
+    words = list(filter((lambda x: x != ''), words))
+
+    print(words)
+    sorted = list(set(words))
+    print(sorted)
+
+    i = 0
+    while i != len(sorted):
+
+        quantity = 0
+        indexes = []
+        index = 0
+
+        for word in words:
+            if word == sorted[i]:
+                indexes.append(words.index(word, index))
+                index += words.index(word, index) + 1
+                quantity += 1
+
+        dic[sorted[i]] = quantity, indexes
+        i += 1
+
+    print(dic)
+
+
 load()
+compress()
+print(dic['fuga'])
+test = enumerate(words)
+print(*test)
