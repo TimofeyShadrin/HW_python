e3e99b599386adc6fc1bca91b90a578590428193
Timofei Shadrin <tshadrin@yandex.ru>
2022-09-20 20:26:32 +0300

    Task 41 of seminar 6 without () are working now

--- sem_06/Task41.py
+++ sem_06/Task41.py
@@ -28,4 +28,50 @@ for char in text:
 
 
 digits = word.split()
+i = 0
+print(digits)
+
+
+size = 0
+for item in digits:
+    if item == '*' or item == '/':
+        size += 1
+size = len(digits) - size * 2
+
+
+while len(digits) != size:
+    if digits[i] == '*':
+        digits[i] = str(round(int(digits[i - 1]) * int(digits[i + 1]), 2))
+        digits.remove(digits[i + 1])
+        digits.remove(digits[i - 1])
+        i = 0
+    elif digits[i] == '/':
+        digits[i] = str(round(int(digits[i - 1]) / int(digits[i + 1]), 2))
+        digits.remove(digits[i + 1])
+        digits.remove(digits[i - 1])
+        i = 0
+    i += 1
+
+
+size = 0
+for item in digits:
+    if item == '+' or item == '-':
+        size += 1
+size = len(digits) - size * 2
+
+
+while len(digits) != size:
+    if digits[i] == '-':
+        digits[i] = str(float(digits[i - 1]) - float(digits[i + 1]))
+        digits.remove(digits[i + 1])
+        digits.remove(digits[i - 1])
+        i = 0
+    elif digits[i] == '+':
+        digits[i] = str(round(float(digits[i - 1]) + float(digits[i + 1]), 2))
+        digits.remove(digits[i + 1])
+        digits.remove(digits[i - 1])
+        i = 0
+    i += 1
+
+
 print(digits)
