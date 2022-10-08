5afea6dc4d1d33f8545a98b7fcf101cf869c4e1e
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-08 13:05:16 +0300

    homework of seminar 11 started

--- sem_11/main.py
+++ sem_11/main.py
@@ -1,13 +1,35 @@
-import psycopg2
+class Person:
 
-conn = psycopg2.connect(dbname='postgres', user='postgres',
-                        password='6773', host='localhost')
-cursor = conn.cursor()
-print(conn.get_dsn_parameters(), "\n")
-cursor.execute('INSERT INTO Customers (Id, '
-               'FirstName, LastName, '
-               'Email, Phone, Age) '
-               "VALUES (2, 'Andrei', 'Sokolov', 'sokolov@yandex.ru', '+7113261233', 29);")
-conn.commit()
-cursor.close()
-conn.close()
+    def __init__(self, f_name, s_name, l_name, tels):
+        self.f_name = f_name
+        self.s_name = s_name
+        self.l_name = l_name
+        self.tels = tels
+
+    def get_phone(self):
+        return self.tels.get('private')
+
+    def get_name(self):
+        return f'{self.f_name} {self.s_name} {self.l_name}'.title()
+
+    def get_work_phone(self):
+        return self.tels.get('work')
+
+    def get_sms_text(self):
+        return f'Уважаемый {self.f_name} {self.s_name}! Примите участие ' \
+               f'в нашем беспроигрышном конкурсе для физических лиц.'
+
+
+class Company:
+
+    def __init__(self, company_name, type_company, tels_company, *persons):
+        self.company_name = company_name
+        self.type_company = type_company
+        self.tels_company = tels_company
+        self.persons = persons
+
+    def get_phone(self):
+        if self.tels_company.get('contact'):
+            return self.tels_company.get('contact')
+        else:
+            return self.persons[0].get('work')
