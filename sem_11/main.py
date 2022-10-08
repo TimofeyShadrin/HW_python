026285ce08709f3b7390ad9f9620c4b92851f71e
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-08 15:24:54 +0300

    homework of seminar 11 finished

--- sem_11/main.py
+++ sem_11/main.py
@@ -32,4 +32,40 @@ class Company:
         if self.tels_company.get('contact'):
             return self.tels_company.get('contact')
         else:
-            return self.persons[0].get('work')
+            return self.persons[0].get_work_phone()
+
+    def get_name(self):
+        return self.company_name
+
+    def get_sms_text(self):
+        return f'Для компании {self.company_name} есть супер предложение! ' \
+               f'Примите участие в нашем беспроигрышном конкурсе для {self.type_company}.'
+
+
+def send_sms(*args):
+    for item in args:
+        if item.get_phone():
+            print(f'Отправлено СМС на номер {item.get_phone()} с текстом: {item.get_sms_text()}')
+        else:
+            print(f'Не удалось отправить сообщение абоненту: {item.get_name()}')
+
+
+# Example 1:
+# person1 = Person('Ivan', 'Ivanovich', 'Ivanov', {'private': 123, 'work': 456})
+# person2 = Person('Ivan', 'Petrovich', 'Petrov', {'private': 789})
+# person3 = Person('Ivan', 'Petrovich', 'Sidorov', {'work': 789})
+# person4 = Person('John', 'Unknown', 'Doe', {})
+# company1 = Company('Bell', 'OOO', {'contact': 111}, person3, person4)
+# company2 = Company('Cell', 'AO', {'non_contact': 222}, person2, person3)
+# company3 = Company('Dell', 'Ltd', {'non_contact': 333}, person2, person4)
+# send_sms(person1, person2, person3, person4, company1, company2, company3)
+
+# Example 2:
+person1 = Person('Степан', 'Петрович', 'Джобсов', {'private': 555})
+person2 = Person('Боря', 'Иванович', 'Гейтсов', {'private': 777, 'work': 888})
+person3 = Person('Семен', 'Робертович', 'Возняцкий', {'work': 789})
+person4 = Person('Леонид', 'Арсенович', 'Тордвальсон', {})
+company1 = Company('Яблочный комбинат', 'OOO', {'contact': 111}, person1, person3)
+company2 = Company('ПластОкно', 'AO', {'non_contact': 222}, person2)
+company3 = Company('Пингвинья ферма', 'Ltd', {'non_contact': 333}, person4)
+send_sms(person1, person2, person3, person4, company1, company2, company3)
